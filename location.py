d8ae3f68f5be948fe79cf086afb984004ea180ab
bradwasher <13262221+bradwasher@users.noreply.github.com>
2019-08-20 13:45:44 -0500

    Add files via upload

---
+++ location.py
@@ -0,0 +1,49 @@
+import gps_poller
+import time
+import threading
+
+class Location:
+
+    def __init__(self, mode, static_lat=0, static_lon=0):
+        print("location here!")
+        self.mode = mode
+        if self.mode == 'static':
+            self.static_latitude = static_lat
+            self.static_longitude = static_lon
+            self.gps_poll = None
+        else:
+            self.static_latitude = None
+            self.static_longitude = None
+            self.last_dynamic_location = {}
+            self.gpsp = gps_poller.GpsPoller()
+
+    def run(self):
+        if self.mode == "dynamic":
+            threading.Thread.__init__(self.gpsp)
+            self.gpsp.start()
+
+    def get_location(self):
+        if self.mode == 'static':
+            location = {'location_mode': 'static', 'latitude': self.static_latitude, 'longitude':  self.static_longitude}
+        else:
+            location = self._get_dynamic_location()
+
+        return location
+
+    def _get_dynamic_location(self):
+        try:
+            loc = self.gpsp.get_location()
+            loc['location_mode'] = 'dynamic'
+            self.last_dynamic_location = loc
+            return loc
+        except:
+            self.last_dynamic_location['location_status'] = 'gps-error'
+            return self.last_dynamic_location
+
+    def stop_polling(self):
+        if self.mode == "dynamic":
+            self.gpsp.stop()
+            self.gpsp.join()
+
+
+
---
+++ webapi.py
@@ -0,0 +1,98 @@
+import requests
+import threading
+import json
+import time
+from datetime import datetime
+
+class WebAPI(threading.Thread):
+    def __init__(self, post_url, jwt="", chunk_size=20, max_checkin_attempts=5, interval=5):
+        threading.Thread.__init__(self)
+        self._running = True
+
+        self.post_url = post_url
+        self.jwt = jwt
+        self.chunk_size = chunk_size
+        self.max_checkin_attempts = max_checkin_attempts
+        self.interval = interval
+        self.queue = []
+
+    def run(self):
+        while self._running:
+            self._process_queue()
+            time.sleep(self.interval)
+
+    def stop(self, process_queue=True):
+        if process_queue:
+            self._process_queue()
+
+        self._running = False
+
+    def check_in(self, settings):
+        settings["record_type"] = "checkin"
+        settings["sensor_utc"] = self.time_stamp()
+
+        results = self._post(self.post_url, settings)
+        counter = 1
+
+        while results.status_code != 200:
+            if counter == self.max_checkin_attempts:
+                return {'status':'failed'}
+            print("Unable to successfully contact collection point - {0} {1}. Trying again....".format(results.status_code, results.text))
+            time.sleep(5)
+            results = self.post(self.post_url, checkin)
+            counter += 1
+
+        if "mode" in settings.keys() and settings["mode"] == 'target-list':
+            tl = results.json()
+            tl['status'] = 'success'
+            return tl
+
+        return {'status':'success'}
+
+    def queue_record(self, record):
+        self.queue.append(record)
+
+
+    def _post(self, url, json_data):
+        headers = {'Authorization': 'Bearer {}'.format(self.jwt)}
+        result = requests.post(url, json=json_data, verify=False, headers=headers)
+
+        return result
+
+
+    def _process_queue(self):
+        count = len(self.queue)
+
+        if count > 0:
+            records = self.queue[0:count]
+            del self.queue[0:count]
+
+            print("processing records: {0}".format(len(records)))
+            chunks = self._get_chunks(records, self.chunk_size)
+            for chunk in chunks:
+                record = {}
+                record["records"] = chunk
+                response = self._post(self.post_url, record)
+                print('{0} - {1}'.format(response.status_code, response.text))
+                #if failure then push back on the queue?
+
+    @staticmethod
+    def _get_chunks(lst, n):
+        groups = []
+        group = []
+        for item in lst:
+            if len(group) >= n:
+                groups.append(group)
+                group = []
+            group.append(item)
+
+        if len(group) > 0:
+            groups.append(group)
+
+        return groups
+
+    @staticmethod
+    def time_stamp():
+        #return f'{datetime.utcnow():%Y-%m-%d %H:%M:%S%z}'
+        return str(datetime.utcnow())
+
